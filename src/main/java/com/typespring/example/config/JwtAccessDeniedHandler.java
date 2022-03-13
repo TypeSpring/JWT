60fb250e6110e25411fc9d2674b87f2c08eed990
Doha <adamdoha@naver.com>
2022-03-14 03:03:28 +0900

    docs: Update README.md

--- src/main/java/com/typespring/example/config/JwtAccessDeniedHandler.java
+++ src/main/java/com/typespring/example/config/JwtAccessDeniedHandler.java
@@ -4,7 +4,6 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -18,7 +17,7 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
             HttpServletRequest request,
             HttpServletResponse response,
             AccessDeniedException accessDeniedException
-    ) throws IOException, ServletException {
+    ) throws IOException {
         //필요한 권한이 없이 접근하려 할때 403
         response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
