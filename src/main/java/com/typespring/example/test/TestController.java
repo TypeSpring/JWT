c6bc3f71af8413e49466cb2b0f868a7851dae442
DolphaGo <adamdoha@naver.com>
2022-04-03 18:37:18 +0900

    Use AuthenticationPrincipal in Controller

--- src/main/java/com/typespring/example/test/TestController.java
+++ src/main/java/com/typespring/example/test/TestController.java
@@ -1,6 +1,7 @@
 package com.typespring.example.test;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,26 +21,29 @@ import lombok.extern.slf4j.Slf4j;
 public class TestController {
 
     @GetMapping
-    public String get() throws IllegalAccessException {
-//        log.info("[TEST] Email = {}", email);
+    public String get(@AuthenticationPrincipal User user) throws IllegalAccessException {
+        log.info("[TEST] Email = {}", user.getUsername());
         final String currentMemberEmail = SecurityUtil.getCurrentMemberEmail();
         return "[GET] " + currentMemberEmail;
     }
 
     @PostMapping
-    public String post() throws IllegalAccessException {
+    public String post(@AuthenticationPrincipal User user) throws IllegalAccessException {
+        log.info("[TEST] Email = {}", user.getUsername());
         final String currentMemberEmail = SecurityUtil.getCurrentMemberEmail();
         return "[POST] " + currentMemberEmail;
     }
 
     @PutMapping
-    public String put() throws IllegalAccessException {
+    public String put(@AuthenticationPrincipal User user) throws IllegalAccessException {
+        log.info("[TEST] Email = {}", user.getUsername());
         final String currentMemberEmail = SecurityUtil.getCurrentMemberEmail();
         return "[PUT] " + currentMemberEmail;
     }
 
     @DeleteMapping
-    public String delete() throws IllegalAccessException {
+    public String delete(@AuthenticationPrincipal User user) throws IllegalAccessException {
+        log.info("[TEST] Email = {}", user.getUsername());
         final String currentMemberEmail = SecurityUtil.getCurrentMemberEmail();
         return "[DELETE] " + currentMemberEmail;
     }
